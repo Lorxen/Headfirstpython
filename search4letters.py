83c48c282424129e80e31dbf41bfbe28692857b2
Lorxen <brianylucia@gmailcom>
2023-02-18 21:53:32 +0100

    windows update

--- search4letters.py
+++ search4letters.py
@@ -1,3 +1,2 @@
-def search4letters(phrase:str, letters:str='aeiou'):
+def search4letters(phrase: str, letters: str = 'aeiou'):
     return set(letters).intersection(set(phrase))
-
--- vsearch4web.py
+++ vsearch4web.py
@@ -3,27 +3,32 @@ from search4letters import search4letters
 
 app = Flask(__name__)
 
-def log_request(req: 'flask_request', res:str):
-    with open ('vsearch.log','a') as write:
-        print(req.form, req.remote_addr, req.user_agent, res, file=write, sep='|')
+
+def log_request(req: 'flask_request', res: str):
+    with open('vsearch.log', 'a') as write:
+        print(req.form, req.remote_addr,
+              req.user_agent, res, file=write, sep='|')
+
 
 @app.route('/search4', methods=['POST'])
 def do_search():
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
-    results = str(search4letters(phrase,letters))
+    results = str(search4letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
+
 @app.route('/viewlog')
 def view_the_log():
-    contents =[]
+    contents = []
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
@@ -32,5 +37,6 @@ def view_the_log():
     titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
